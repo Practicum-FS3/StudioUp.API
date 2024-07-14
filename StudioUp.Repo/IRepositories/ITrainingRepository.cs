d78dfa921cfbfede5cd462387ac1f36b020c5c41
practicum-fs3 <practicumfs3@gmail.com>
2024-07-14 15:49:51 +0300

    create customerRepositiry

---
+++ StudioUp.Repo/IRepositories/ITrainingRepository.cs
@@ -0,0 +1,18 @@
+﻿using StudioUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioUp.Repo.IRepositories
+{
+    public interface ITrainingRepository
+    {
+        public Task<IEnumerable<Training>> GetAllTrainings();
+        public Task<Training> GetTrainingById(int id);
+        public Task AddTraining(Training training);
+        public Task UpdateTraining(Training training);
+        public Task DeleteTraining(int id);
+    }
+}
