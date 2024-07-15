3d104db57adb0e4d60a72780561782b658becebc
BatyaMamman <batyam5014@gmail.com>
2024-07-15 10:06:59 +0300

    Rep&Controllers - Customers

---
+++ StudioUp.Repo/IRepositories/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudioUp.Models;
+
+namespace StudioUp.Repo.IRepositories
+{
+    public interface ICustomerRepository
+    {
+        Task<List<Customer>> GetAllAsync();
+        Task<Customer> GetByIdAsync(int id);
+        Task<bool> UpdateAsync(Customer entity);
+        Task<int> AddAsync(Customer entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ StudioUp.Repo/Repositories/CustomerRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using StudioUp.Models;
+using StudioUp.Repo.IRepositories;
+
+namespace StudioUp.Repo.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly DataContext context;
+        public async Task<int> AddAsync(Customer entity)
+        {
+            try
+            {
+                var newCustomer = await context.Customers.AddAsync(entity);
+                await context.SaveChangesAsync();
+                return newCustomer.Entity.Id;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            try
+            {
+                var c = await GetByIdAsync(id);
+                context.Customers.Remove(c);
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<Customer>> GetAllAsync()
+        {
+            try
+            {
+                return await context.Customers.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return new List<Customer>();
+            }
+
+        }
+
+        public async Task<Customer> GetByIdAsync(int id)
+        {
+            try
+            {
+                var c = await context.Customers.FirstOrDefaultAsync(t => t.Id == id);
+                return c;
+
+            }
+            catch (Exception ex)
+            {
+                return new Customer();
+            }
+        }
+
+        public async Task<bool> UpdateAsync(Customer entity)
+        {
+            try
+            {
+                var customerToUpdate = await GetByIdAsync(entity.Id);
+                if (customerToUpdate == null)
+                {
+                    return false;
+                }
+                context.Customers.Update(customerToUpdate);
+                await context.SaveChangesAsync();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
