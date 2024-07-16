4a287ee6ee017bb76c3eaa96e35bc85d8d959923
BatyaMamman <batyam5014@gmail.com>
2024-07-16 12:45:54 +0300

    Is Active in DTO

--- StudioUp.DTO/TrainerDTO.cs
+++ StudioUp.DTO/TrainerDTO.cs
@@ -12,7 +12,7 @@ namespace StudioUp.DTO
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        //public bool IsActive { get; set; }
+        public bool IsActive { get; set; }
         public string Mail { get; set; }
         public string Tel { get; set; }
         public string Adress { get; set; }
