9f5e3a22736eea0966ac3780cfe34e712f994ebc
Feigi Horen <fmhoren@gmail.com>
2024-08-05 21:25:55 +0300

    ..

--- StudioUp.API/Program.cs
+++ StudioUp.API/Program.cs
@@ -115,29 +115,29 @@ namespace StudioUp.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(options =>
             {
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Name = "Authorization",
-                    Description = "Bearer Authentication with JWT Token",
-                    Type = SecuritySchemeType.Http
-                });
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Id = "Bearer",
-                                Type = ReferenceType.SecurityScheme
-                            }
-                        },
-                        new List<string>()
-                    }
-                });
+                //options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                //{
+                //    Scheme = "Bearer",
+                //    BearerFormat = "JWT",
+                //    In = ParameterLocation.Header,
+                //    Name = "Authorization",
+                //    Description = "Bearer Authentication with JWT Token",
+                //    Type = SecuritySchemeType.Http
+                //});
+                //options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                //{
+                //    {
+                //        new OpenApiSecurityScheme
+                //        {
+                //            Reference = new OpenApiReference
+                //            {
+                //                Id = "Bearer",
+                //                Type = ReferenceType.SecurityScheme
+                //            }
+                //        },
+                //        new List<string>()
+                //    }
+                //});
             });
 
             builder.Services.AddLogging();
