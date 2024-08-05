78c7cdb3cf5c10fe95f3f6f8b354f9ead03b72e1
Your Name <you@example.com>
2024-08-05 13:54:36 +0300

    myChanges

--- StudioUp.API/Controllers/CustomerSubscriptionController.cs
+++ StudioUp.API/Controllers/CustomerSubscriptionController.cs
@@ -1,92 +1,92 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using StudioUp.DTO;
-using StudioUp.Models;
-using StudioUp.Repo.IRepositories;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using StudioUp.DTO;
+//using StudioUp.Models;
+//using StudioUp.Repo.IRepositories;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
 
-namespace StudioUp.API.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CustomerSubscriptionsController : ControllerBase
-    {
-        private readonly ICustomerSubscriptionRepository _repository;
+//namespace StudioUp.API.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class CustomerSubscriptionsController : ControllerBase
+//    {
+//        private readonly ICustomerSubscriptionRepository _repository;
 
-        public CustomerSubscriptionsController(ICustomerSubscriptionRepository repository)
-        {
-            _repository = repository;
-        }
+//        public CustomerSubscriptionsController(ICustomerSubscriptionRepository repository)
+//        {
+//            _repository = repository;
+//        }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<CustomerSubscriptionDTO>>> GetAllCustomerSubscriptions()
-        {
-            var subscriptions = await _repository.GetAllCustomerSubscriptionsAsync();
-            // המרה ל-DTO או הוספת אוטומאפר במקרה שזה קיים
-            return Ok(subscriptions);
-        }
+//        [HttpGet]
+//        public async Task<ActionResult<IEnumerable<CustomerSubscriptionDTO>>> GetAllCustomerSubscriptions()
+//        {
+//            var subscriptions = await _repository.GetAllCustomerSubscriptionsAsync();
+//            // המרה ל-DTO או הוספת אוטומאפר במקרה שזה קיים
+//            return Ok(subscriptions);
+//        }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<CustomerSubscriptionDTO>> GetCustomerSubscriptionById(int id)
-        {
-            var subscription = await _repository.GetCustomerSubscriptionByIdAsync(id);
-            if (subscription == null)
-            {
-                return NotFound();
-            }
-            // המרה ל-DTO במקרה הצורך
-            return Ok(subscription);
-        }
+//        [HttpGet("{id}")]
+//        public async Task<ActionResult<CustomerSubscriptionDTO>> GetCustomerSubscriptionById(int id)
+//        {
+//            var subscription = await _repository.GetCustomerSubscriptionByIdAsync(id);
+//            if (subscription == null)
+//            {
+//                return NotFound();
+//            }
+//            // המרה ל-DTO במקרה הצורך
+//            return Ok(subscription);
+//        }
 
-        [HttpPost]
-        public async Task<ActionResult> AddCustomerSubscription(CustomerSubscriptionDTO subscriptionDTO)
-        {
-            // המרת DTO לאובייקט המודל
-            var subscription = new CustomerSubscription
-            {
-                CustomerID = subscriptionDTO.CustomerID,
-                SubscriptionTypeId = subscriptionDTO.SubscriptionTypeId,
-                StartDate = subscriptionDTO.StartDate
-            };
+//        [HttpPost]
+//        public async Task<ActionResult> AddCustomerSubscription(CustomerSubscriptionDTO subscriptionDTO)
+//        {
+//            // המרת DTO לאובייקט המודל
+//            var subscription = new CustomerSubscription
+//            {
+//                CustomerID = subscriptionDTO.CustomerID,
+//                SubscriptionTypeId = subscriptionDTO.SubscriptionTypeId,
+//                StartDate = subscriptionDTO.StartDate
+//            };
 
-            await _repository.AddCustomerSubscriptionAsync(subscription);
-            return CreatedAtAction(nameof(GetCustomerSubscriptionById), new { id = subscription.ID }, subscription);
-        }
+//            await _repository.AddCustomerSubscriptionAsync(subscription);
+//            return CreatedAtAction(nameof(GetCustomerSubscriptionById), new { id = subscription.ID }, subscription);
+//        }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateCustomerSubscription(int id, CustomerSubscriptionDTO subscriptionDTO)
-        {
-            if (id != subscriptionDTO.ID)
-            {
-                return BadRequest();
-            }
+//        [HttpPut("{id}")]
+//        public async Task<IActionResult> UpdateCustomerSubscription(int id, CustomerSubscriptionDTO subscriptionDTO)
+//        {
+//            if (id != subscriptionDTO.ID)
+//            {
+//                return BadRequest();
+//            }
 
-            var subscription = new CustomerSubscription
-            {
-                ID = subscriptionDTO.ID,
-                CustomerID = subscriptionDTO.CustomerID,
-                SubscriptionTypeId = subscriptionDTO.SubscriptionTypeId,
-                StartDate = subscriptionDTO.StartDate
-            };
+//            var subscription = new CustomerSubscription
+//            {
+//                ID = subscriptionDTO.ID,
+//                CustomerID = subscriptionDTO.CustomerID,
+//                SubscriptionTypeId = subscriptionDTO.SubscriptionTypeId,
+//                StartDate = subscriptionDTO.StartDate
+//            };
 
-            try
-            {
-                await _repository.UpdateCustomerSubscriptionAsync(subscription);
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                return NotFound();
-            }
+//            try
+//            {
+//                await _repository.UpdateCustomerSubscriptionAsync(subscription);
+//            }
+//            catch (DbUpdateConcurrencyException)
+//            {
+//                return NotFound();
+//            }
 
-            return NoContent();
-        }
+//            return NoContent();
+//        }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCustomerSubscription(int id)
-        {
-            await _repository.DeleteCustomerSubscriptionAsync(id);
-            return NoContent();
-        }
-    }
-}
+//        [HttpDelete("{id}")]
+//        public async Task<IActionResult> DeleteCustomerSubscription(int id)
+//        {
+//            await _repository.DeleteCustomerSubscriptionAsync(id);
+//            return NoContent();
+//        }
+//    }
+//}
